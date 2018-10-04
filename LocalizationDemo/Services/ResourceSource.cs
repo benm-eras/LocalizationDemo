6834898fad223fd77ef8bc64bdc78abb3f12b443
Ben Miller <BenM@eras.co.uk>
2018-10-04 12:49:04 +0100

    added potential solution using explicit scope

--- LocalizationDemo/Services/ResourceSource.cs
+++ LocalizationDemo/Services/ResourceSource.cs
@@ -1,23 +1,39 @@
 ﻿using LocalizationDemo.Data;
+using Microsoft.Extensions.DependencyInjection;
 using System.Linq;
 
 namespace LocalizationDemo.Services
 {
     public class ResourceSource : IResourceSource
     {
-        private readonly DataContext context;
-        //private readonly DataContextAccessor contextAccessor;
+        private readonly IServiceScopeFactory scopes;
 
-        public ResourceSource(DataContext context) => this.context = context;
-        //public ResourceSource(DataContextAccessor contextAccessor) => this.contextAccessor = contextAccessor;
+        public ResourceSource(IServiceScopeFactory scopes) => this.scopes = scopes;
 
         public string Get(string key)
         {
-            // get stuff from db
-            //int n = this.contextAccessor.Context.Foos.Count();
-            int n = this.context.Foos.Count();
+            using (IServiceScope scope = this.scopes.CreateScope())
+            using (DataContext context = scope.ServiceProvider.GetRequiredService<DataContext>())
+            {
+                int n = context.Foos.Count();
 
-            return $"[{key}-{n}]";
+                return $"[{key}-{n}]";
+            }
         }
+
+        //private readonly DataContext context;
+        ////private readonly DataContextAccessor contextAccessor;
+
+        //public ResourceSource(DataContext context) => this.context = context;
+        ////public ResourceSource(DataContextAccessor contextAccessor) => this.contextAccessor = contextAccessor;
+
+        //public string Get(string key)
+        //{
+        //    // get stuff from db
+        //    //int n = this.contextAccessor.Context.Foos.Count();
+        //    int n = this.context.Foos.Count();
+
+        //    return $"[{key}-{n}]";
+        //}
     }
 }
